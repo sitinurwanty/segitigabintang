5ee127cbe1be8c157301231c1b51b9cf693c3ebd
sitinurwanty <sitinurwanty@gmail.com>
2016-11-04 22:38:24 +0700

    12141385 rumus segitiga sama kaki, Seperti yang kita ketahui segitiga sama kaki adalah segitiga yang memiliki panjang kaki kiri sama dengan kaki kanan

--- segitiga.java
+++ segitiga.java
@@ -28,6 +28,42 @@ class segitiga {
             }
         System.out.println();
         }
-       
+        
+        // Segitiga kanan atas
+        System.out.println("Segitiga Kanan Atas");
+        for(i=0; i<=panjang; i++) {
+            for(j=0; j<(panjang-i); j++) {
+                System.out.print(" ");
+            }
+            for(int k=0; k<i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        
+        // Segitiga kanan bawah
+        System.out.println("Segitiga Kanan Bawah");
+        for(i=0; i<panjang; i++) {
+            for(int k=0; k<i; k++) {
+                System.out.print(" ");
+            }
+            for(j=0; j<(panjang-i); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        // Segitigas sama kaki 
+        System.out.println("Sama Kaki");
+        for(i=0; i<=panjang; i++) {
+            for (int m=1; m<=panjang-i; m++) {
+                System.out.print (" ");
+            }
+            for (int m=1; m<2*i; m++) {
+                System.out.print ("*");
+            }
+        System.out.println (" ");
+        }
     }
 }
